b8d189fba98801c5c5e14add3d4349ecd3b6cdc5
Japhet Kuntu Blankson <japhetkuntublankson1@gmail.com>
2022-09-29 22:47:10 +0000

    chore(profile):added description to wallet profile class

--- Hubtel.Wallets.Api/Profiles/WalletProfile.cs
+++ Hubtel.Wallets.Api/Profiles/WalletProfile.cs
@@ -1,4 +1,14 @@
-﻿using AutoMapper;
+﻿/**
+* @author Japhet Kuntu Blankson
+* 
+* @createdOn  29 September 2022
+* 
+* @fileName - WalletProfile.cs
+* 
+* @Copyright (c) 2022 HUBTEL
+*/
+
+using AutoMapper;
 using Hubtel.Wallets.Api.Dtos;
 using Hubtel.Wallets.Api.Models;
 
