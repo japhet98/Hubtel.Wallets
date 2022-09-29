d5737025126ea8090800d0edb01bbb6e15866e6b
Japhet Kuntu Blankson <japhetkuntublankson1@gmail.com>
2022-09-29 12:43:45 +0000

    feat(interface): added wallet interface

---
+++ Hubtel.Wallets.Api/Interfaces/IWalletRepo.cs
@@ -0,0 +1,26 @@
+﻿/**
+* @author Japhet Kuntu Blankson
+* 
+* @createdOn  28 September 2022
+* 
+* @fileName - IWalletRepo.cs
+* 
+* @Copyright (c) 2022 HUBTEL
+*/
+
+using Hubtel.Wallets.Api.Models;
+using System.Collections.Generic;
+
+namespace Hubtel.Wallets.Api.Interfaces
+{
+    public interface IWalletRepo
+    {
+        void CreateWallet(Wallet _wallet);
+        void DeleteWallet(Wallet _wallet);
+
+        Wallet GetWalletById(int _walletId);
+
+        IEnumerable<Wallet> GetAllWallets();
+
+    }
+}
