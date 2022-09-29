c47dfe038af7a31d49b7fac4042bb3d53e10e930
Japhet Kuntu Blankson <japhetkuntublankson1@gmail.com>
2022-09-29 12:53:27 +0000

    feat(dbContext):added wallet db context

---
+++ Hubtel.Wallets.Api/Data/WalletDBContext.cs
@@ -0,0 +1,24 @@
+﻿
+
+using Hubtel.Wallets.Api.Models;
+using Microsoft.EntityFrameworkCore;
+/**
+* @author Japhet Kuntu Blankson
+* 
+* @createdOn  28 September 2022
+* 
+* @fileName - WalletDBContext.cs
+* 
+* @Copyright (c) 2022 HUBTEL
+*/
+namespace Hubtel.Wallets.Api.Data
+{
+    public class WalletDBContext : DbContext
+    {
+        public WalletDBContext(DbContextOptions<WalletDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Wallet> Wallets { get; set; }
+    }
+}
