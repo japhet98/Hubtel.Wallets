4fe61342b769f39643bbf09531c71b721478c7f3
Japhet Kuntu Blankson <japhetkuntublankson1@gmail.com>
2022-09-29 14:14:31 +0000

    feat(Dto): added wallet dtos

---
+++ Hubtel.Wallets.Api/Dtos/WalletReadDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hubtel.Wallets.Api.Dtos
+{
+    public class WalletReadDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string WalletType { get; set; }
+        public string AccountNumber { get; set; }
+        public string Owner { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
